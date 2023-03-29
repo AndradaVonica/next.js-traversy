973776c367b501a52a0e73808d7b4851ea4951d0
Andrada Vonica <vonicaandradaiulia@gmail.com>
2023-03-29 11:13:43 +0300

    Register changes

--- pages/account/register.js
+++ pages/account/register.js
@@ -86,7 +86,7 @@ export default function registerPage() {
                             onChange={ (e) => setPasswordConfirm(e.target.value) } >
                         </input>
                     </div>
-                    <input type='submit' value='Login' className='btn' />
+                    <input type='submit' value='Register' className='btn' />
                 </form>
                 <p> Already have an account?
                     <Link href="/account/login" legacyBehavior>
